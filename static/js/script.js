22f54b7d7804f338ebb76e70af90c54c518cdeb1
Killol14 <kgselectrical.plumbing@gmail.com>
2023-05-01 14:47:59 +0000

    this will commit added tasks and date picker on new places category

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,16 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-    $('.collapsible').collapsible();
+    $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
-
 
 
