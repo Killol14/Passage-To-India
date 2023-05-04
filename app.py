b4ff136fe56e3a8a119b69d287d1c6a3e9c7d74c
Killol14 <kgselectrical.plumbing@gmail.com>
2023-05-04 13:12:23 +0000

    this will commit new image folder inplace

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
-
 if os.path.exists("env.py"):
     import env
 
@@ -19,9 +18,9 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 client = MongoClient(os.environ.get("MONGO_URI"))
 db = client[os.environ.get("MONGO_DBNAME")]
-
 
 @app.route("/")
 @app.route("/get_tasks")
@@ -40,7 +39,7 @@ def search():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check if username already exists 
+        # check if username already exists
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -55,7 +54,6 @@ def register():
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
